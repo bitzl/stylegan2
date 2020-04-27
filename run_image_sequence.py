665bbca419b1af9258b5b10c24ac320f6958cdd3
Marcus Bitzl <marcus@bitzl.io>
2020-04-28 00:08:12 +0200

    Add debugging info

--- run_image_sequence.py
+++ run_image_sequence.py
@@ -95,7 +95,7 @@ def generate_movie_batch(network_pkl, zs_path, truncation_psi, batch_size):
 
     def update(frame_index):
         if len(buffer) == 0:
-            print("Fill buffer:", z_idx, "of", len(zs), "-", frame_index)
+            print(f"{frame_index:08d} Refill buffer ({z_idx} of {len(zs)})")
             z = zs[z_idx:batch_size]
             z_idx = z_idx + batch_size
             rnd = np.random.RandomState(1000)
@@ -108,7 +108,7 @@ def generate_movie_batch(network_pkl, zs_path, truncation_psi, batch_size):
     animation = FuncAnimation(
         fig,
         update,
-        range(zs),
+        range(len(zs)),
         fargs=[],
         interval=1000 / 25
     )
