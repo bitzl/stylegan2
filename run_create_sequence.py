3bd17f38e09fe0cf7e66d5f7ebecad90b5453e74
Marcus Bitzl <marcus@bitzl.io>
2020-05-01 20:58:35 +0200

    Add straight line sequence

--- run_create_sequence.py
+++ run_create_sequence.py
@@ -20,6 +20,11 @@ Run 'python %(prog)s <subcommand> --help' for subcommand help.''',
     parser_generate_images.add_argument('--speed', type=float, help='Max variance per dimension between two steps (default: %(default)s)', default=0.01, required=False)
     parser_generate_images.add_argument('--target', type=str, help='Target array file (.npy)', required=True)
 
+    parser_generate_images = subparsers.add_parser('line', help='Create a straight line')
+    parser_generate_images.add_argument('--min_length', type=float, default=1.0, help='Minimal length of the straight line', required=False)
+    parser_generate_images.add_argument('--speed', default=0.01, type=float, help='Max variance per dimension between two steps (default: %(default)s)', required=False)
+    parser_generate_images.add_argument('--target', type=str, help='Target array file (.npy)', required=True)
+
     args = parser.parse_args()
     kwargs = vars(args)
     subcmd = kwargs.pop('command')
@@ -35,12 +40,33 @@ Run 'python %(prog)s <subcommand> --help' for subcommand help.''',
 
     if subcmd == "random-walk":
         generate_random_walk(target, kwargs.pop("length"), kwargs.pop("speed"))
+    elif subcmd == "line":
+        generate_line(target, kwargs.pop("min_length"), kwargs.pop("speed"))
 
 
 def generate_random_walk(target: str, length: int, speed: float):
     latent_size = 512
     x = np.random.normal(scale=speed, size=(length, latent_size))
     np.save(target, x.cumsum(axis=1))
+
+
+def generate_line(target: str, min_length: int, speed: float):
+    latent_size = 512
+    z0 = np.random.normal(size=(latent_size))
+    distance = 0
+    guesses = 0
+    while distance < min_length and guesses < 50:
+        z1 = np.random.normal(size=(latent_size))
+        distance = np.linalg.norm(z1 - z0)
+        guesses += 1
+    
+    if distance < min_length:
+        print(f"Could not guess a line with minimum length {min_length}, giving up")
+        return
+
+    steps = int(distance / speed)
+    np.save(target, np.linspace(z0, z1, steps))
+    print(f"Saved line with {steps} points to {target}")
 
 
 if __name__ == "__main__":
